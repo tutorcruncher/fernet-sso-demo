3b8a523873811c84e6edf991d2972073b0a234be
Samuel Colvin <s@muelcolvin.com>
2016-05-28 20:26:54 +0100

    add docstrings to functions

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ redis_cli = Redis()
 
 @app.route('/')
 def index_view():
+    """
+    Route page which either shows a simple message if you're not logged in, or shows a log of
+    recent events (eg. logins) to authenticated users.
+    """
     group = session.get('group')
     if group:
         group_key = 'info:{}'.format(group)
@@ -30,6 +34,9 @@ def index_view():
 
 @app.route('/profile')
 def profile_view():
+    """
+    Display details about an authenticated user's session.
+    """
     profile = session.get('profile')
     if not profile:
         return 'Permission Denied: you can sign in using TutorCruncher SSO', 403
@@ -38,6 +45,9 @@ def profile_view():
 
 @app.route('/add-group', methods=['GET', 'POST'])
 def add_group_view():
+    """
+    Create a new "group" for SSO. Groups are simply a name and a secret key.
+    """
     if request.method == 'POST':
         group = re.sub(r'\W', '', request.form['group-name'])
         group_key = 'secret:{}'.format(group)
@@ -52,7 +62,9 @@ def add_group_view():
 
 @app.route('/sso-lander/<group>')
 def sso_lander_view(group):
-
+    """
+    Log a user into the system using a signed and encrypted get argument "token".
+    """
     secret = redis_cli.get('secret:{}'.format(group))
     if not secret:
         return 'group not found', 404
@@ -81,6 +93,9 @@ def sso_lander_view(group):
 
 @app.route('/logout')
 def logout():
+    """
+    Log the user out.
+    """
     session.pop('profile')
     session.pop('group')
     return redirect('/')
